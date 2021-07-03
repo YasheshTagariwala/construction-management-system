334f9a6e2cff564731839aa88e04030f818b2a84
thekishanmodi <kishan.modi1995@gmail.com>
2021-07-03 22:18:28 +0530

    Added Header footer components

---
+++ client/src/layouts/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="container bg-blue-500">
+            Header
+        </div>
+    )
+}
+
+export default Header;
---
+++ client/src/pages/landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+    return (
+        <div className="container">
+            Landing
+        </div>
+    )
+}
+
+export default Landing;
