24801f0df7d67d77a2fa55dcad9138eea2fbcb89
Yashesh Tagariwala <tagariwalayashesh@gmail.com>
2021-09-29 22:33:50 +0530

    Minor fixes

--- client/src/layouts/header.jsx
+++ client/src/layouts/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import avatar from "../assets/images/avatar.png"
 import {logoutUser} from "../redux/auth/actions";
 import {connect} from "react-redux";
@@ -13,23 +13,53 @@ import {
 import {DropdownDivider, DropdownHeader} from "semantic-ui-react";
 
 function Header(props) {
+    const [notificationToggle, setNotificationToggle] = useState(false);
+    const [profileToggle, setProfileToggle] = useState(false);
+    window.addEventListener('resize', (function () {
+        window.width < 768 && document.querySelector(".sidebar .collapse").classList.toggle("hide")
+    }))
+
+    document.addEventListener("scroll", (function () {
+        let element = document.querySelector(".scroll-to-top");
+        if (document.documentElement.scrollTop > 100) {
+            element.classList.remove('d-none');
+            element.classList.add('d-block');
+        } else {
+            element.classList.remove('d-block');
+            element.classList.add('d-none');
+        }
+    }))
+    const toggleSidebar = () => {
+        document.body.classList.toggle("sidebar-toggled");
+        document.querySelector(".sidebar").classList.toggle("toggled")
+        document.querySelector(".sidebar").classList.contains("toggled") && document.querySelector(".sidebar .collapse").classList.toggle("hide");
+    }
+
+    const scrollToUp = (e) => {
+        document.querySelector("html, body").scrollTo({
+            top: 0,
+            behavior: 'smooth',
+            left: 0
+        });
+        e.preventDefault()
+    }
+
     return (
         <React.Fragment>
             <Navbar expand light className="topbar mb-4 static-top">
-                <button id="sidebarToggleTop" className="btn btn-link rounded-circle ml-3">
+                <button id="sidebarToggleTop" onClick={() => toggleSidebar()}
+                        className="btn btn-link rounded-circle ml-3">
                     <i className="fa fa-bars primary-color"/>
                 </button>
                 <ul className="navbar-nav ml-auto">
-                    <Dropdown tag="li" className="no-arrow mr-2 nav-item">
-                        <NavLink className="dropdown-toggle" href="#" id="alertsDropdown"
-                                 aria-haspopup="true" aria-expanded="false"
-                                 role="button" data-toggle="dropdown">
+                    <Dropdown isOpen={notificationToggle} toggle={() => setNotificationToggle(prevState => !prevState)}
+                              tag="li" className="no-arrow mr-2 nav-item cursor-pointer">
+                        <DropdownToggle tag="a" className="nav-link dropdown-toggle" id="alertsDropdown">
                             <i className="fas fa-bell fa-fw primary-color"/>
                             <span className="badge badge-danger badge-counter">3+</span>
-                        </NavLink>
-                        <DropdownMenu right className="dropdown-list shadow animated--grow-in"
-                                      aria-labelledby="alertsDropdown">
-                            <DropdownHeader>Alerts Center</DropdownHeader>
+                        </DropdownToggle>
+                        <DropdownMenu right className="dropdown-list shadow animated--grow-in">
+                            <DropdownHeader tag="h6" className="dropdown-header">Alerts Center</DropdownHeader>
                             <DropdownItem className="d-flex align-items-center" href="#">
                                 <div className="mr-3">
                                     <div className="icon-circle primary-background">
@@ -71,8 +101,11 @@ function Header(props) {
                             </DropdownItem>
                         </DropdownMenu>
                     </Dropdown>
-                    <Dropdown tag="li" className="no-arrow nav-item" toggle={() => null}>
-                        <DropdownToggle tag="a" className="nav-link" role="button" data-toggle="dropdown" aria-expanded="false"
+                    <Dropdown tag="li" className="no-arrow nav-item" isOpen={profileToggle}
+                              toggle={() => setProfileToggle(prevState => !prevState)}>
+                        <DropdownToggle tag="a" className="nav-link dropdown-toggle" role="button"
+                                        data-toggle="dropdown"
+                                        aria-expanded="false"
                                         id="userDropdown" href="#">
                             <img alt="." className="img-profile rounded-circle" src={avatar} style={{
                                 maxWidth: '60px'
@@ -83,7 +116,7 @@ function Header(props) {
                                 <h6 className="m-0 primary-color">Erica Jordon</h6>
                                 <p className="m-0 text-gray-600">Admin</p>
                             </div>
-                            <DropdownDivider/>
+                            <DropdownDivider className="dropdown-divider"/>
                             <DropdownItem className="px-4 py-2" href="#">
                                 <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-600"/>
                                 Profile
@@ -92,7 +125,8 @@ function Header(props) {
                                 <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-600"/>
                                 Settings
                             </DropdownItem>
-                            <DropdownItem className="px-4 py-2" href="#" onClick={() => props.logoutUser(props.history)}>
+                            <DropdownItem className="px-4 py-2" href="#"
+                                          onClick={() => props.logoutUser(props.history)}>
                                 <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-600"/>
                                 Logout
                             </DropdownItem>
@@ -100,7 +134,7 @@ function Header(props) {
                     </Dropdown>
                 </ul>
             </Navbar>
-            <div className="scroll-to-top rounded">
+            <div className="scroll-to-top rounded" onClick={(e) => scrollToUp(e)}>
                 <i className="fas fa-angle-up"/>
             </div>
         </React.Fragment>
--- client/src/layouts/sidebar.jsx
+++ client/src/layouts/sidebar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import favicon from '../assets/images/favicon.png'
-import {NavItem, NavLink} from "reactstrap";
+import {Collapse, NavItem, NavLink} from "reactstrap";
 
 function SideBar(props) {
+    const [isInspectionOpen, setIsInspectionOpen] = useState(false);
     return (
         <ul className="navbar-nav sidebar accordion" id="accordionSidebar">
             <Link className="sidebar-brand d-flex align-items-center" to="/">
@@ -12,26 +13,27 @@ function SideBar(props) {
                 </div>
                 <div className="sidebar-brand-text ml-2">C360 LABS</div>
             </Link>
-            <NavItem className="active">
+            {/*className="active"*/}
+            <NavItem>
                 <NavLink href={`/${props.user?.role}/dashboard`}>
                     <i className="fas fa-fw fa-tachometer-alt"/>
                     <span>Dashboard</span>
                 </NavLink>
             </NavItem>
-            {/*TODO: FIX OPEN CLOSE*/}
             <NavItem>
-                <NavLink className="collapsed" href="#" data-toggle="collapse" data-target="#collapsePage"
-                         aria-expanded="true" aria-controls="collapsePage">
+                <NavLink className={`${!isInspectionOpen ? 'collapsed' : ''}`} href="#" data-toggle="collapse"
+                         data-target="#collapsePage"
+                         aria-expanded="true" aria-controls="collapsePage"
+                         onClick={() => setIsInspectionOpen(prevState => !prevState)}>
                     <i className="fas fa-fw fa-columns"/>
                     <span>Inspection</span>
                 </NavLink>
-                <div id="collapsePage" className="collapse" aria-labelledby="headingPage"
-                     data-parent="#accordionSidebar">
+                <Collapse isOpen={isInspectionOpen}>
                     <div className="primary-light-background  py-2 collapse-inner rounded">
+                        <a className="collapse-item" href={`/${props.user?.role}/inspection`}>Inspections</a>
                         <a className="collapse-item" href={`/${props.user?.role}/inspection/add`}>Add Inspection</a>
-                        {/*<a className="collapse-item" href="inspectionDetail.php">Inspection Detail</a>*/}
                     </div>
-                </div>
+                </Collapse>
             </NavItem>
         </ul>
     )
